2945dcc318d768814f61bea7e9dd6199f5b804eb
thorvald <thorvald@gridinsoft.com>
2022-08-16 14:48:19 +0300

    Update ths-monit-php-fpm and functions

--- bin/ths-monit-php-fpm.sh
+++ bin/ths-monit-php-fpm.sh
@@ -2,58 +2,55 @@
 
 THS_PATH="/usr/local/thscripts"
 
+# Config format for work with this script is CHECK_URL="url1;php-backend1 url2;php-backend2 ..."
+# Also need add to config url of telegram bot TELEG_URL and chat id TELEG_CHAT_ID
 . ${THS_PATH}/etc/global.conf
 . ${THS_PATH}/etc/functions.sh
 
-if [[ -e /var/lock/monit-php-fpm.lock ]]
-    then
-        PID=`cat /var/lock/monit-php-fpm.lock`
-        ps aux | awk '{ print $2 }' | grep ${PID} > /dev/null
-        if [[ $? -eq 0 ]]
-            then
-                exit 0
-        fi
+code_check() {
+ http_code=$(curl -I -s -S --connect-timeout 40 "${1}" | grep "HTTP/" | awk '{ print $2 }' | cut -c -1)
+ if $?; then
+   return $http_code
+ else
+   return 000
+ fi
+}
+
+if [[ -e /var/lock/monit-php-fpm.lock ]]; then
+  PID=$(cat /var/lock/monit-php-fpm.lock)
+#  ps aux | awk '{ print $2 }' | grep "${PID}" >/dev/null
+  if ps aux | awk '{ print $2 }' | grep "${PID}"; then
+    exit 0
+  fi
+else
+  echo $$ >/var/lock/monit-php-fpm.lock
+fi
+
+for i in $CHECK_URL; do
+  url_f_check=${i//*;/}
+  url_backnd=${i//;*/}
+  counter=1
+  date=$(date '+%F-%H:%M')
+  while True; do
+    return_code=$(code_check "$url_f_check")
+    if [ ${return_code} -eq 5 ]; then
+      /bin/systemctl stop ${url_backnd}-fpm.service &>/dev/null
+      sleep 10
+      /bin/systemctl start "${url_backnd}"-fpm.service &>/dev/null
+      echo "service php-fpm has been restarted on $date at $counter time" >>/root/monit-php-fpm.log
+    elif [ "${return_code}" -eq 000 ]; then
+      :
     else
-        echo $$ > /var/lock/monit-php-fpm.lock
-fi
-
-if [[ -n ${1} ]]
-    then
-        CHECK_URL=${1}
+      break
+    fi
+    if [ ${counter} -lt 5 ]; then
+      counter=$((counter + 1))
     else
-        echo "You must set url for monitoring"
-        exit 1
-fi
-
-http_code=`curl -I -s -S --connect-timeout 40 ${CHECK_URL} | grep "HTTP/" | awk '{ print $2 }' | cut -c -1`
-counter=1
-date=`date '+%F-%H:%M'`
-
-while [[ ${http_code} -eq 5 && ${counter} -lt 5 ]]
-    do
-        /bin/systemctl stop php-fpm.service &> /dev/null
-        sleep 10
-        /bin/systemctl start php-fpm.service &> /dev/null
-        http_code=`curl -I -s -S --connect-timeout 40 ${CHECK_URL} | grep "HTTP/" | awk '{ print $2 }' | cut -c -1`
-        echo "service php-fpm has been restarted on $date at $counter time" >> /root/monit-php-fpm.log
-        counter=$(($counter+1))
-        sleep 20
-    done
-
-if [[ ${http_code} -eq 5 ]]
-    then
-        /bin/systemctl stop mariadb &> /dev/null
-        sleep 10
-        /bin/systemctl start mariadb &> /dev/null
-        sleep 20
-        http_code=`curl -I -s -S --connect-timeout 40 ${CHECK_URL} | grep "HTTP/" | awk '{ print $2 }' | cut -c -1`
-        echo "service mariadb has been restarted on $date" >> /root/monit-php-fpm.log
-fi
-
-if [[ ${http_code} -eq 5 ]]
-        then
-                echo "service php-fpm can't work after $counter restart" | mail -s "PHP-fpm on ${CHECK_URL}" $MAIL
-fi
+      publ_ip=$(get_publ_ip)
+      teleg_alert "$TELEG_URL" "$TELEG_CHAT_ID" "Php backend problems on $publ_ip"
+    fi
+  done
+done
 
 rm /var/lock/monit-php-fpm.lock
 exit 0
--- etc/functions.sh
+++ etc/functions.sh
@@ -1,64 +1,64 @@
 #!/usr/bin/env bash
 
 __nginx_conf_paths() {
-chck_path="$1"
-includes=`cat ${chck_path} | egrep "(\s*|\t*)include.*" | awk '{ print $2 }' | sed -r 's/;$//'`
-for i in ${includes}
-    do
-        dname=`dirname ${i} | head -n1`
-        if [[ "${dname}" = "." ]]
-            then
-                dname=`dirname ${chck_path}`
+  chck_path="$1"
+  includes=$(cat "${chck_path}" | grep -E "(\s*|\t*)include.*" | awk '{ print $2 }' | sed -r 's/;$//')
+  for i in ${includes}; do
+    dname=$(dirname "${i}" | head -n1)
+    if [[ "${dname}" = "." ]]; then
+      dname=$(dirname "${chck_path}")
+    fi
+    fname_tmp=$(basename "${i}")
+    confs=$(find "${dname}" -type f -name "$fname_tmp" print0 | xargs)
+    check=$(echo "$confs" | wc -w)
+    if [[ ${check} -eq 1 ]]; then
+      CONF_LIST="$CONF_LIST $confs"
+      fname=$(basename "${i}")
+      full_fname="${dname}/$fname"
+      cat "${full_fname}" | grep -E "(\s*|\t*)include.*" >/dev/null
+      if [[ $? -eq 0 ]]; then
+        __nginx_conf_paths "${full_fname}"
+      fi
+    else
+      for j in ${confs}; do
+        CONF_LIST="${CONF_LIST} ${confs}"
+        fname=$(basename "${j}")
+        full_fname="${dname}/$fname"
+        if [[ $? -eq 0 ]]; then
+          __nginx_conf_paths "${full_fname}"
         fi
-        fname_tmp=`basename ${i}`
-        confs=`find ${dname} -type f -name "$fname_tmp" | xargs`
-        check=`echo "$confs" | wc -w`
-        if [[ ${check} -eq 1 ]]
-            then
-                CONF_LIST="$CONF_LIST $confs"
-                fname=`basename ${i}`
-                full_fname="${dname}/$fname"
-                cat ${full_fname} | egrep "(\s*|\t*)include.*" > /dev/null
-                if [[ $? -eq 0 ]]
-                    then
-                        __nginx_conf_paths ${full_fname}
-                fi
-            else
-                for j in ${confs}
-                    do
-                        CONF_LIST="${CONF_LIST} ${confs}"
-                        fname=`basename ${i}`
-                        full_fname="${dname}/$fname"
-                        if [[ $? -eq 0 ]]
-                            then
-                                __nginx_conf_paths ${full_fname}
-                        fi
-                    done
-        fi
-done
+      done
+    fi
+  done
 }
 
 nginx_conf_paths() {
-__nginx_conf_paths ${1}
-for i in ${CONF_LIST}; do echo ${i}; done | sort -u | xargs
+  __nginx_conf_paths "${1}"
+  for i in ${CONF_LIST}; do echo "${i}"; done | sort -u | xargs
 }
 
 vesta_usr_list() {
-for USER in `VESTA=/usr/local/vesta /usr/local/vesta/bin/v-list-users plain | awk '{ print $1 }' | xargs`
-    do
-        grep "${USER}" /etc/passwd &>/dev/null
-        if [[ $? -eq 0 ]]
-            then
-                vst_users="${vst_users} ${USER}"
-        fi
-    done
-echo ${vst_users}
+  for USER in $(VESTA=/usr/local/vesta /usr/local/vesta/bin/v-list-users plain | awk '{ print $1 }' | xargs); do
+    grep "${USER}" /etc/passwd &>/dev/null
+    if [[ $? -eq 0 ]]; then
+      vst_users="${vst_users} ${USER}"
+    fi
+  done
+  echo "${vst_users}"
 }
 
 vesta_domain_list() {
-VESTA=/usr/local/vesta /usr/local/vesta/bin/v-list-web-domains ${1} plain | awk '{ print $1 }' | xargs
+  VESTA=/usr/local/vesta /usr/local/vesta/bin/v-list-web-domains "${1}" plain | awk '{ print $1 }' | xargs
 }
 
 get_publ_ip() {
-dig +short myip.opendns.com @resolver1.opendns.com
+  dig +short myip.opendns.com @resolver1.opendns.com
+}
+
+teleg_alert() {
+  tg_url=$1
+  tg_chat_id=$2
+  shift 2
+  send_messg="\`\`\`$*\`\`\`"
+  curl -X POST "$tg_url" -H 'Content-Type: application/json' -d "{'chat_id':$tg_chat_id,'text':$send_messg,'parse_mode':'Markdown','type':'pre'}"
 }
