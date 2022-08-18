3f2253d36585f9deff05875203f66a0527ab3d93
thorvald <thorvald@gridinsoft.com>
2022-08-18 18:42:20 +0300

    Update ths-monit-php-fpm and functions

--- bin/ths-monit-php-fpm.sh
+++ bin/ths-monit-php-fpm.sh
@@ -8,7 +8,7 @@ THS_PATH="/usr/local/thscripts"
 . ${THS_PATH}/etc/functions.sh
 
 code_check() {
- if http_code=$(curl -s -o /dev/null -I -w "%{http_code}" --connect-timeout 40 "${1}" | grep "HTTP/" | awk '{ print $2 }' | cut -c -1; true); then
+ if http_code=$(curl -s -o /dev/null -I -w "%{http_code}" --connect-timeout 40 "${1}"; true); then
    echo "$http_code"
  else
    echo 000
