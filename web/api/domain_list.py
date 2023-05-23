3108b4823daa71d71ad3e8badf2b0bc3f4dc6d8b
thorv <thorvaldr.hrafn@outlook.com>
2023-05-23 15:44:29 +0300

    Add cf dns

--- web/api/domain_list.py
+++ web/api/domain_list.py
@@ -40,6 +40,7 @@ def nginx_inc_grep(chk_conf, dname="", list_confs=None):
                 for line in ngnx_file:
                     if re.match('(\\s*|\t*)include.*', line):
                         file_mask = re.sub('(\\s*|\t*)include(\\s+|\t+)', '', line, count=1)
+                        print(file_mask)
                         file_mask = re.sub(';\n', '', file_mask, count=1)
                         tmp_file_list.append(file_mask)
                         for f in tmp_file_list:
